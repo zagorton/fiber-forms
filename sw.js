5cf61a07ef292141580dd21740c3d6c9f8c3ff13
zagorton <135411623+zagorton@users.noreply.github.com>
2026-09-01 19:22:04 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE = "fiber-forms-v29";
+const CACHE = "fiber-forms-v31";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./privacy.html",
-                "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+                "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png",
+                "./maskable-192.png", "./maskable-512.png"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
