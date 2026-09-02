ed17320b707c38bb40e2827a4e11c076e7159621
zagorton <135411623+zagorton@users.noreply.github.com>
2026-09-01 18:54:53 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fiber-forms-v28";
+const CACHE = "fiber-forms-v29";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./privacy.html",
                 "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
